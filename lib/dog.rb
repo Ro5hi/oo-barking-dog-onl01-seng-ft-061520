e8195888e412abd13e5ae73d2780d0bdba84474c
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-18 21:01:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,15 @@
 class Dog
-  def name= "Fido"
-    this_dogs_name = Fido
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
  
+  def name
+    @this_dogs_name
+  end
+end
+ 
+fido = Dog.new
+fido.name = "Fido"
+ 
+puts lassie.name
